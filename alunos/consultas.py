e74800958e6c98e604ebb9467b85ba352d546a47
raffindev <arch.rsouza@gmail.com>
2026-09-08 22:03:08 -0300

    feat: adiciona consultas de alunos

--- alunos/consultas.py
+++ alunos/consultas.py
@@ -10,3 +10,35 @@ def verificar_id_aluno(alunos, novo_aluno):
             return True
         
     return False
+
+def listar_alunos(alunos):
+
+    for aluno in alunos:
+        print(
+            f'\nID: {aluno["id"]}'
+            f'\nNome: {aluno["nome"]}'
+        )
+
+        if aluno["ativo"]:
+            print('Status: Ativo')
+
+        else:
+            print('Status: Inativo')
+        print('-'*25)
+
+def buscar_aluno_por_id(alunos):
+    while True:
+        try:
+            id_aluno = int(input("ID: "))
+            if id_aluno > 0:
+                break
+
+        except ValueError:
+            print("ID Inválido")
+
+    for aluno in alunos:
+        if id_aluno == aluno["id"]:
+            return aluno
+        
+    return "Aluno não encontrado"
+
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+# Teste de cadastro.
+
 from utils.arquivos import ler_json, salvar_json
 from alunos.cadastro import cadastrar_aluno, base_aluno
+from alunos.consultas import listar_alunos
 
 
 alunos = ler_json("dados/alunos.json")
 lista_cadastro = ler_json("dados/cadastro.json")
+listar_alunos(alunos)
 
 cadastro = cadastrar_aluno(lista_cadastro, alunos)
 
