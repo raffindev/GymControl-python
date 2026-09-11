0f0e316b799c0812c07a84b0b5252a7ab2876ff9
raffindev <arch.rsouza@gmail.com>
2026-09-11 00:34:47 -0300

    fix: corrige fluxo de cadastro de aluno

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # Teste do programa temporariamente
 
-from utils.arquivos import carregar_dados
+from utils.arquivos import carregar_dados, salvar_json
 from alunos.cadastro import cadastrar_base, cadastrar_aluno
-from utils.arquivos import salvar_json
+
 
 alunos, funcionarios = carregar_dados()
-
 base = cadastrar_base(alunos, funcionarios)
 alunos.append(base)
+
 cadastro = cadastrar_aluno(base["id"], alunos, funcionarios)
-salvar_json("dados/alunos.json", alunos)
+salvar_json("dados/indice_alunos.json", alunos)
